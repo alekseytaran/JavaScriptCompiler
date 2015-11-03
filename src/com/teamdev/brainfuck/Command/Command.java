bfc198a984e3715023e66869a207637a212a92ab
aleksey.taran <subaru13>
2015-11-03 13:45:21 +0200

    Renamed package

--- src/com/teamdev/brainfuck/Command/Command.java
+++ src/com/teamdev/brainfuck/Command/Command.java
@@ -3,6 +3,7 @@ package com.teamdev.brainfuck.command;
 import com.teamdev.brainfuck.CommandVisitor;
 
 public interface Command {
+
     void accept(CommandVisitor visitor);
 
     void incrementCountRepeat();
